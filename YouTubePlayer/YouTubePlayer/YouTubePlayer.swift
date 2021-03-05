7df2250237df69f442dff53cb4ddddc39c37db47
Marcel Tuchner <marcel.tuchner@worldiety.de>
2021-03-05 07:11:40 +0100

    Change playerHTMLPath to support SPM Ressource Module loading

--- YouTubePlayer/YouTubePlayer/YouTubePlayer.swift
+++ YouTubePlayer/YouTubePlayer/YouTubePlayer.swift
@@ -253,7 +253,7 @@ open class YouTubePlayerView: UIView, WKNavigationDelegate {
     }
 
     fileprivate func playerHTMLPath() -> String {
-        return Bundle(for: YouTubePlayerView.self).path(forResource: "YTPlayer", ofType: "html")!
+        return Bundle.module.path(forResource: "YTPlayer", ofType: "html")!
     }
 
     fileprivate func htmlStringWithFilePath(_ path: String) -> String? {
